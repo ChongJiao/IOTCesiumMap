21c7ab6a6e61de2d79c014cf31cb85c7ddbf404c
ChongJiao <840436263@qq.com>
2020-06-29 16:09:36 +0800

    Four url test

--- src/components/CesiumMap.vue
+++ src/components/CesiumMap.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="viewer">
-    <vc-viewer @ready="ready" style="background-image: url(http://127.0.0.1:8000/static/assets/mapBack.png)">
+    <vc-viewer @ready="ready">
       <vc-layer-imagery :alpha="alpha" :imageryProvider="imageryProvider" :brightness="brightness" :contrast="contrast">
       </vc-layer-imagery>
       <vc-layer-imagery :alpha="alpha" :brightness="brightness" :contrast="contrast" v-if="showTileMap">
         <vc-provider-imagery-tile-mapservice
-          :url="tileUrl"
+          :url="tileUrl1"
+          @readyPromise="imageryReady"
+        ></vc-provider-imagery-tile-mapservice>
+      </vc-layer-imagery>
+      <vc-layer-imagery :alpha="alpha" :brightness="brightness" :contrast="contrast" v-if="showTileMap">
+        <vc-provider-imagery-tile-mapservice
+          :url="tileUrl2"
+          @readyPromise="imageryReady"
+        ></vc-provider-imagery-tile-mapservice>
+      </vc-layer-imagery>
+      <vc-layer-imagery :alpha="alpha" :brightness="brightness" :contrast="contrast" v-if="showTileMap">
+        <vc-provider-imagery-tile-mapservice
+          :url="tileUrl3"
+          @readyPromise="imageryReady"
+        ></vc-provider-imagery-tile-mapservice>
+      </vc-layer-imagery>
+      <vc-layer-imagery :alpha="alpha" :brightness="brightness" :contrast="contrast" v-if="showTileMap">
+        <vc-provider-imagery-tile-mapservice
+          :url="tileUrl4"
           @readyPromise="imageryReady"
         ></vc-provider-imagery-tile-mapservice>
       </vc-layer-imagery>
@@ -19,6 +37,22 @@ export default {
     tileUrl: {
       type: String,
       default: 'http://localhost:8000/GFData/tileData/GF1_PMS2_E113.8_N30.5_20190524_L1A0004018806'
+    },
+    tileUrl1: {
+      type: String,
+      default: 'http://localhost:8000/GFData/tileData/GF1_PMS2_E113.8_N30.5_20190524_L1A0004018806-pansharpen-0'
+    },
+    tileUrl2: {
+      type: String,
+      default: 'http://localhost:8000/GFData/tileData/GF1_PMS2_E113.8_N30.5_20190524_L1A0004018806-pansharpen-1'
+    },
+    tileUrl3: {
+      type: String,
+      default: 'http://localhost:8000/GFData/tileData/GF1_PMS2_E113.8_N30.5_20190524_L1A0004018806-pansharpen-2'
+    },
+    tileUrl4: {
+      type: String,
+      default: 'http://localhost:8000/GFData/tileData/GF1_PMS2_E113.8_N30.5_20190524_L1A0004018806-pansharpen-3'
     },
     showTileMap: {
       type: Boolean,
